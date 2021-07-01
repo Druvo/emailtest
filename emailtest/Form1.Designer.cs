09510ed41e18258600e3a20af38d280e70dc2452
Zahid Hasan <zhdruvo@gmail.com>
2021-07-01 11:02:08 +0600

    Initial commit

---
+++ emailtest/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace emailtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.host = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.port = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.to = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.message = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.subject = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // host
+            // 
+            this.host.Location = new System.Drawing.Point(140, 18);
+            this.host.Margin = new System.Windows.Forms.Padding(4);
+            this.host.Name = "host";
+            this.host.Size = new System.Drawing.Size(305, 22);
+            this.host.TabIndex = 0;
+            this.host.Text = "smtp.gmail.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SMTPHost";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SMTPUserName";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(140, 50);
+            this.username.Margin = new System.Windows.Forms.Padding(4);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(305, 22);
+            this.username.TabIndex = 2;
+            this.username.Text = "bloodlagbebd@gmail.com";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 118);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "SMTPPort";
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(140, 114);
+            this.port.Margin = new System.Windows.Forms.Padding(4);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(305, 22);
+            this.port.TabIndex = 6;
+            this.port.Text = "587";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 86);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "SMTPPassword";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(140, 82);
+            this.password.Margin = new System.Windows.Forms.Padding(4);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(305, 22);
+            this.password.TabIndex = 4;
+            this.password.Text = "bloodlagbe321";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 150);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "TO";
+            // 
+            // to
+            // 
+            this.to.Location = new System.Drawing.Point(140, 146);
+            this.to.Margin = new System.Windows.Forms.Padding(4);
+            this.to.Name = "to";
+            this.to.Size = new System.Drawing.Size(305, 22);
+            this.to.TabIndex = 8;
+            this.to.Text = "zhdruvo@gmail.com";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 267);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(60, 219);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Message";
+            // 
+            // message
+            // 
+            this.message.Location = new System.Drawing.Point(140, 210);
+            this.message.Margin = new System.Windows.Forms.Padding(4);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(305, 22);
+            this.message.TabIndex = 11;
+            this.message.Text = "TEST from code";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(69, 178);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Subject";
+            // 
+            // subject
+            // 
+            this.subject.Location = new System.Drawing.Point(140, 178);
+            this.subject.Margin = new System.Windows.Forms.Padding(4);
+            this.subject.Name = "subject";
+            this.subject.Size = new System.Drawing.Size(305, 22);
+            this.subject.TabIndex = 13;
+            this.subject.Text = "TEST MSG";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 325);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.subject);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.to);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.host);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Email Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox host;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox to;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox message;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox subject;
+    }
+}
+
---
+++ emailtest/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace emailtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string SMTPHost = host.Text;
+                string SMTPUserName = username.Text;
+                string SMTPPassword = password.Text;
+                string address = to.Text;
+                string FromEmail = username.Text;
+                int SMTPPort = Convert.ToInt32(port.Text.ToString());
+                MailMessage mail = new MailMessage(FromEmail, address);
+                mail.Subject = subject.Text;
+                mail.Body = message.Text;
+                mail.IsBodyHtml = true;
+                using (var client = new SmtpClient(SMTPHost, SMTPPort))
+                {
+                    client.EnableSsl = true;
+                    //client.UseDefaultCredentials = false;
+                    client.Credentials = new NetworkCredential(SMTPUserName, SMTPPassword);
+                    client.Send(mail);
+                }
+                MessageBox.Show("Email sent!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            
+
+        }
+    }
+}
